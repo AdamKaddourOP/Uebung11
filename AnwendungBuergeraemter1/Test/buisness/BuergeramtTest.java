d4cd9885a6834c75385fed0c0d48393fe8b93916
Adamddour <106589116+Adamddour@users.noreply.github.com>
2024-01-10 22:13:06 +0100

    ->22:12

--- AnwendungBuergeraemter1/Test/buisness/BuergeramtTest.java
+++ AnwendungBuergeraemter1/Test/buisness/BuergeramtTest.java
@@ -15,13 +15,13 @@ public class BuergeramtTest {
 
 	    @BeforeEach
 	    void ausfuellen() throws Exception {
-	        buergeramt = new Buergeramt("B�rgerb�ro", 9.00f, 17.00f, "Querenburg H�he 256", new String[]{"Hauptwohnsitz", "Nebenwohnsitz"});
+	        buergeramt = new Buergeramt("B�rgerb�ro", 9.00f, 17.00f, "Querenburg H�he 256", new String[]{"Hauptwohnsitz", "Nebenwohnsitz"});
 	    }
 	    @Test
 	    void TextBuergeramt() {
 	    	assertTrue(()->this.buergeramt.getGeoeffnetVon()==9.00f);
 	    	assertTrue(this.buergeramt.getGeoeffnetBis()==17.00f);
-	    	assertTrue(this.buergeramt.getStrasseHNr().equals("Querenburg H�he 256"));
+	    	assertTrue(this.buergeramt.getStrasseHNr().equals("Querenburg H�he 256"));
 	    	
 //	    	ArrayList<String> dienstleistungen = buergeramt.getDienstleistungen();
 //	        assertEquals(2, dienstleistungen.size());
@@ -30,3 +30,73 @@ public class BuergeramtTest {
 	        
 	    }
 }
+
+
+
+
+
+
+
+
+
+/*
+ * 
+ * package business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.BooleanSupplier;
+
+import org.junit.jupiter.api.Test;
+
+class TestKonstruktorBuergeramt1 {
+
+
+	@Test
+	void test() {
+		String[] dienstleistung = new String[] {"Hauptwohmsitz", "Nebenwohnsitz"};  
+		Buergeramt buergeramt = new Buergeramt("Bürgerbüro Querenburg", 9.00f, 17.00f, "Querenburger Höhe 256", dienstleistung);
+		
+		BooleanSupplier isNameCorrect = () ->  {
+			if( buergeramt.getName() == "Bürgerbüro Querenburg") {
+				return true;
+
+			}
+			return false; 
+		};
+		assertTrue(isNameCorrect.getAsBoolean());
+	}
+
+}
+ * 
+ * */
+
+
+
+/*
+ * 
+ * 
+ * package business;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestKonstruktorFehlermeldung {
+
+
+	@Test
+	void test() {
+		IllegalArgumentException t =  assertThrows(IllegalArgumentException.class, () ->  {new Buergeramt("Bürgerbüro Querenburg", 9.00f, 17.00f, "Querenburger Höhe 256", null);});	
+		assertEquals(t.getMessage(), "Dienstleistung is null");
+	}
+
+}
+
+ * 
+ * 
+ * */
+
+
+
